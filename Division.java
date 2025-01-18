592e5958c3ff7a160f5734382e827f3580d11272
ayush-singh787898 <ayushsingh05086@gmail.com>
2025-01-18 09:36:15 +0530

    Add Division Code

---
+++ Division.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Division {
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter first Number");
+        int x=sc.nextInt();
+        System.out.println("Enter Second Number");
+
+        int y=sc.nextInt();
+        System.out.println("Enter the character");
+
+        char ch=sc.next().charAt(0);
+        if(ch=='/'){
+            System.out.println(x/y);
+        }
+    }
+    
+}
+
